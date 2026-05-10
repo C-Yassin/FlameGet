73e7b5b790ab63da44fa4cfc65006aef63d2123f
Yassin <55513401+C-Yassin@users.noreply.github.com>
2026-05-10 22:18:55 +0100

    Update browser_context_menu_handler.py

--- browser_context_menu_handler.py
+++ browser_context_menu_handler.py
@@ -3,8 +3,8 @@ import sys
 import os
 import subprocess
 import threading
+os.environ['GSK_RENDERER'] = 'cairo'
 import gi
-
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk, GLib, Gio
 import argparse
